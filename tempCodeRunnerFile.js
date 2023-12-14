7ca84f270f743604d2a5e790bdcb73df309829c8
Cemal Sonat Alicik <cemalsonatalicik@Cemals-MacBook-Pro.local>
2023-12-14 22:04:45 +0200

    Initial commit

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js';
+import { getAnalytics } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js';
